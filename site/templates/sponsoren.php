fc7fe2fb1198e13e0ea3ffbe5393548ff0da3cfd
Magnus Buk <info@magnusbuk.de>
2018-04-11 14:49:45 +0200

    sponsoren seite

---
+++ site/templates/sponsoren.php
@@ -0,0 +1,29 @@
+<?php snippet('header'); ?>
+<section class="main-banner text-xs-center text-sm-center text-md-left">
+    <div class="container text-lite-color pl-sm-0 pr-sm-0">
+        <h1 class="text-weight-medium"><?= $page->title();?></h1>
+    </div>
+</section>
+<div class="page">
+    <div class="main-container container pl-sm-0 pr-sm-0">
+        <h2 class="main-heading-1 text-spl-color text-weight-normal text-center-xs"><?= $page->headline();?></h2>
+        <?= $page->text()->kirbytext();?>
+        <div class="row mt-5">
+            <?php $box = false; $last = ''; foreach ($page->sponsoren()->toStructure()->sortBy('name', 'asc') as $sponsor): $current = strtoupper(substr($sponsor->name(), 0, 1)); ?>
+                <?php if ($last != $current): ?>
+                    <?php if ($box): ?>
+                    </div>
+                    <?php endif; ?>
+                    <div class="col-12 col-md-6 col-lg-4 mb-4 mb-md-4">
+                    <h3 class="main-heading-1 text-spl-color text-weight-normal text-center-xs mb-3 mb-md-5"><?= strtoupper($current);?></h3>
+                <?php endif; ?>
+                <?php if ($sponsor->webseite()->isNotEmpty()):?>
+                    <a href="<?= $sponsor->webseite();?>" alt="Zur Webseite: <?= $sponsor->name();?> <?= $sponsor->ort();?>" title="Zur Webseite: <?= $sponsor->name();?> <?= $sponsor->ort();?>"><?= $sponsor->name();?></a><br/><small><i><?= $sponsor->ort();?></i></small><br/>
+                <?php else: ?>
+                    <?= $sponsor->name();?><br/><small><i><?= $sponsor->ort();?></i></small><br/>
+                <?php endif; ?>
+            <?php $last = $current; $box = true; endforeach; ?>
+        </div>
+    </div>
+</div>
+<?php snippet('footer');?>
