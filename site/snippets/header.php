d32771972eee23064012fc219365c97e08ab12c5
Magnus Buk <info@magnusbuk.de>
2018-10-30 15:30:38 +0100

    new styles

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -36,8 +36,8 @@
 <header class="main-header">
     <div class="container text-xs-center pl-sm-0 pr-sm-0">
         <div class="row">
-            <div class="col-lg-4 col-md-5 col-sm-12 text-xs-center text-sm-center text-md-left">
-                <a href="/"><img src="/assets/images/sfb/Sportfreunde_Bronnen_Logo_1.svg" alt="Sportfreunde Bronnen Logo" class="pulse img-center-xs logo"></a>
+            <div class="col-lg-10 col-sm-12 text-xs-center text-sm-center text-md-left logo">
+                <a href="/">Sportfreunde Bronnen 1949 e.V.</a>
             </div>
         </div>
     </div>
