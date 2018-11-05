315ccd25654ba0b06b4ed2e1e155bee0b0dc99db
Magnus Buk <info@magnusbuk.de>
2018-11-05 14:29:45 +0100

    added tracktics tracking page

---
+++ site/templates/stats.php
@@ -0,0 +1,34 @@
+<?php snippet('header'); ?>
+<section class="main-banner text-xs-center text-sm-center text-md-left">
+    <div class="container text-lite-color pl-sm-0 pr-sm-0">
+        <h1 class="text-weight-medium"><?= $page->title();?></h1>
+    </div>
+</section>
+<div class="page">
+    <div class="main-container container pl-sm-0 pr-sm-0">
+        <h2 class="main-heading-1 text-spl-color text-weight-normal text-center-xs"><?= $page->headline();?></h2>
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th>Datum</th>
+                <th>Spiel</th>
+                <th>Spieler</th>
+                <th>Mannschaft</th>
+                <th>&nbsp;</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php foreach ($page->children() as $statsPage): ?>
+                <tr>
+                    <td class="text-weight-bold"><?= date('d.m.Y', $statsPage->date());?></td>
+                    <td><?= $statsPage->spieler();?></td>
+                    <td><?= $statsPage->gegner();?></td>
+                    <td><?= ucfirst($statsPage->category());?></td>
+                    <td><a href="<?= $statsPage->url();?>" class="btn btn-1">Einsehen</a></td>
+                </tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+</div>
+<?php snippet('footer');?>
---
+++ site/templates/tracktics.php
@@ -0,0 +1,19 @@
+<?php snippet('header'); ?>
+<section class="main-banner text-xs-center text-sm-center text-md-left">
+    <div class="container text-lite-color pl-sm-0 pr-sm-0">
+        <h1 class="text-weight-medium">Tracktics Stats</h1>
+    </div>
+</section>
+<div class="page">
+    <div class="main-container container pl-sm-0 pr-sm-0">
+        <h2 class="main-heading-1 text-spl-color text-weight-normal text-center-xs"><?= $page->spieler();?> vs. <?= $page->gegner();?> (<?= date('d.m.Y', $page->date());?>)</h2>
+        <?php if ($page->images()): ?>
+            <div class="text-center">
+                <?php foreach($page->images() as $image): ?>
+                    <img src="<?= $image->resize(750)->url();?>" class="img-fluid mb-2"/>
+                <?php endforeach; ?>
+            </div>
+        <?php endif; ?>
+    </div>
+</div>
+<?php snippet('footer');?>
