e051c3ab89b98c14cf9b6ce802702435d081b8de
Magnus Buk <info@magnusbuk.de>
2018-11-12 14:52:13 +0100

    added gymnastik api

---
+++ site/templates/gymapi.php
@@ -0,0 +1,27 @@
+<?php
+
+header('Content-type: application/json; charset=utf-8');
+
+$result = [];
+
+foreach ($site->index()->visible()->filterBy('template', 'kurs') as $course):
+
+    $courseData = [];
+    $courseData['title'] = (string)$course->title()->text();
+    $courseData['text'] = (string)strip_tags($course->text()->markdown());
+    $courseData['times'] = (string)trim($course->zeiten());
+    $courseData['place'] = (string)$course->ort();
+    $courseData['type'] = (string)$course->parent()->title();
+    $courseData['images'] = [];
+    
+    if ($course->images()->count() > 0):
+        foreach ($course->images()->limit(2) as $image):
+            $courseData['images'][] = $image->url();
+        endforeach;
+    endif;
+
+    $result[] = $courseData;
+
+endforeach;
+
+echo json_encode($result);
