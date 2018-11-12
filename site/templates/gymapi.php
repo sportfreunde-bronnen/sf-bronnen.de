b30aaccd3075e58a816043c895eecc08760fe923
Magnus Buk <info@magnusbuk.de>
2018-11-12 15:28:27 +0100

    added id to the courses

--- site/templates/gymapi.php
+++ site/templates/gymapi.php
@@ -7,13 +7,14 @@ $result = [];
 foreach ($site->index()->visible()->filterBy('template', 'kurs') as $course):
 
     $courseData = [];
+    $courseData['id'] = (string)($course->alexaId()->isEmpty() ? $course->title()->text() : $course->alexaId());
     $courseData['title'] = (string)$course->title()->text();
     $courseData['text'] = (string)strip_tags($course->text()->markdown());
     $courseData['times'] = (string)trim($course->zeiten());
     $courseData['place'] = (string)$course->ort();
     $courseData['type'] = (string)$course->parent()->title();
     $courseData['images'] = [];
-    
+
     if ($course->images()->count() > 0):
         foreach ($course->images()->limit(2) as $image):
             $courseData['images'][] = $image->url();
