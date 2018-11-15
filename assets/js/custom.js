7fe2926545da18adeb264d6b2f78fa19abe527d7
Magnus Buk <info@magnusbuk.de>
2018-11-15 15:46:09 +0100

    added rudimentary counter logic

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -108,6 +108,24 @@
           .addClass("fa-plus");
   });
 
+  // Gallery FILTERS
+  var $grid = $('#gallery-grid');
+  $grid.shuffle({
+    itemSelector: '.gallery-grid-item', // the selector for the items in the grid
+    speed: 500 // Transition/animation speed (milliseconds)
+  });
+
+  /* reshuffle when user clicks a filter item */
+  $('#gallery-filter li a').click(function (e) {
+    // set active class
+    $('#gallery-filter li a').removeClass('active');
+    $(this).addClass('active');
+    // get group name from clicked item
+    var groupName = $(this).attr('data-group');
+    // reshuffle grid
+    $grid.shuffle('shuffle', groupName );
+  });
+
   //MAGNIFIC POPUP
   $('#gallery-grid').magnificPopup({
       delegate: 'a', 
