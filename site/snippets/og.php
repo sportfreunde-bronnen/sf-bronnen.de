3e533f66a61f788ad67b4c3d1873216b1e0bac6c
Magnus Buk <info@magnusbuk.de>
2019-03-18 16:07:57 +0100

    og fix

--- site/snippets/og.php
+++ site/snippets/og.php
@@ -3,7 +3,7 @@ if ($page->template() === 'bericht') {
 
     foreach (explode(',', $page->textImages()) as $imageUrl):
         $image = $page->images()->find($imageUrl);
-        echo sprintf('<meta property="og:image" content="%s">', $image->resize(1500)->url());
+        echo sprintf('<meta property="og:image:secure_url" content="%s">', $image->resize(1500)->url());
     endforeach;
 
 }
