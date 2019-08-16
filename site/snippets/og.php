9b3aced4d2b62d492fcaf88a1efe53c2ab4e0d92
Magnus Buk <info@magnusbuk.de>
2019-08-16 11:08:34 +0200

    double meta tag

--- site/snippets/og.php
+++ site/snippets/og.php
@@ -6,7 +6,6 @@ if ($page->template() === 'bericht') {
         echo sprintf('<meta property="og:title" content="%s"/>', $page->title());
         echo sprintf('<meta property="og:type" content="%s"/>', "article");
         echo sprintf('<meta property="og:url" content="%s"/>', $page->url());
-        echo sprintf('<meta property="og:url" content="%s"/>', $page->url());
         echo sprintf('<meta property="og:description" content="%s"/>', substr($page->text(), 0, 300) . '...');
         echo sprintf('<meta property="og:image" content="%s"/>', $image->resize(1500)->url());
         echo sprintf('<meta property="og:image:secure_url" content="%s"/>', $image->resize(1500)->url());
