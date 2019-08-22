4c196a11729c8423b41146041df19b6aa292eccb
Magnus Buk <info@magnusbuk.de>
2019-08-22 14:49:27 +0200

    Set custom propertie after pageload

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,9 +2,11 @@
 
   "use strict";
 
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+
   // We listen to the resize event
   window.addEventListener('resize', () => {
-    console.log("HIER BIN ICH!");
     // We execute the same script as before
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
