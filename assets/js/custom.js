f94f6d4b7886372a7fc3e82d48adf001bb75ea0a
Magnus Buk <info@magnusbuk.de>
2019-08-22 14:40:02 +0200

    Add fix vor vh propertie issue

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,6 +2,13 @@
 
   "use strict";
 
+  // We listen to the resize event
+  window.addEventListener('resize', () => {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
+
   if (window.innerWidth >= 768) {
 
     $(window).scroll(function () {
