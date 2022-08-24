041d7bc20c00c052352eb2f19195df4f2a6814e1
Magnus Buk <info@magnusbuk.de>
2022-08-24 13:35:26 +0200

    yodeck/fupa adaption

--- site/templates/mannschaft.php
+++ site/templates/mannschaft.php
@@ -5,9 +5,18 @@
             <h2 class="main-heading-1 text-spl-color text-weight-normal text-center text-lg-left"><?= $page->headline();?></h2>
             <div class="row">
                 <div class="col-12">
-                    <?= $page->fupawidget();?>
+                    <?php
+                    $fupaTab = $_GET['fupaTab'] ?? 'default';
+                    $fupaData = $page->fupawidget();
+                    if ($fupaTab !== 'default') {
+                      echo str_replace('act:"team"', 'act:"' . $fupaTab . '"', $fupaData);
+                    } else {
+                      echo $fupaData;
+                    }
+                    ?>
                 </div>
             </div>
         </div>
     </div>
-<?php snippet('footer'); ?><?php snippet('close'); ?>
+<?php snippet('footer'); ?>
+<?php snippet('close'); ?>
