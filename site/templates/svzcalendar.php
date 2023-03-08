18b8a395abc78148560bd13139e4f18617b1cc7e
Magnus Buk <info@magnusbuk.de>
2023-03-08 16:25:20 +0100

    Fixes

--- site/templates/svzcalendar.php
+++ site/templates/svzcalendar.php
@@ -4,10 +4,10 @@
     <div class="main-container container pl-sm-0 pr-sm-0">
         <h2 class="main-heading-1 text-spl-color text-weight-normal text-center text-lg-left"><?= $page->headline();?></h2>
         <div class="row">
-        <div class="col-12 col-lg-8">
+        <div class="col-12">
             <?= $page->text()->kirbytext();?>
         </div>
-        <div class="col-12 col-lg-4">
+        <div class="col-12">
             <?= $page->facts()->kirbytext();?>
         </div>
         <div class="col-12">
